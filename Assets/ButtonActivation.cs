668391efd92ceb8b42843c7cd32d458847f1c061
Teodor Todorov <teditods13@gmail.com>
2022-10-24 02:36:41 +0300

    Initial commit

---
+++ Assets/ButtonActivation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivation : MonoBehaviour
+{
+    public GameObject active;
+    public GameObject inactive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        inactive.SetActive(false);
+        active.SetActive(true);
+    }
+}
